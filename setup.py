1cc9a4a318c51bcaa20ac5224c06efc829895985
yahia boudah <yahiabouda@hotmail.com>
2022-01-03 20:56:13 +0100

    modify setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-
+import json
 from setuptools import setup
 
-with open("README", 'r') as f:
-    long_description = f.read()
+with open('README.md', 'r') as R, open('SETUP.json', 'r') as S:
+    cfg = json.loads(S.read())
+    cfg["long_description"] = R
 
-setup(
-
-   name='pyes',
-   version='0.1',
-   description='Extendscript to Python bridge',
-   license="GNU",
-   long_description=long_description,
-   author='Yahia Boudah',
-   author_email='yahiabouda@hotmail.com',
-   url="",
-   packages=['pyes'], #same as name
-   install_requires=[], #external packages as dependencies
-   scripts=[]
-)
+setup(**cfg)
